956b91909d2b8fda0b4c472d1f0eb8c6d0b26f02
Serine230 <serinekhelifi05@gmail.com>
2026-04-19 17:05:20 +0100

    feat: SessionManager terminé

---
+++ src/main/java/com/coachia/utils/SessionManager.java
@@ -0,0 +1,29 @@
+package com.coachia.utils;
+
+import com.coachia.model.User;
+
+public class SessionManager {
+
+    // L'utilisateur connecté gardé en mémoire
+    private static User currentUser = null;
+
+    // Connecter un utilisateur
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    // Récupérer l'utilisateur connecté
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    // Vérifier si quelqu'un est connecté
+    public static boolean isLoggedIn() {
+        return currentUser != null;
+    }
+
+    // Déconnecter
+    public static void logout() {
+        currentUser = null;
+    }
+}
